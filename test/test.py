9b50ba192b739180d1a4abdaec1ead1ad397b93b
phile13 <47676503+phile13@users.noreply.github.com>
2020-05-07 12:03:16 -0400

    Initial Setup and Testing

---
+++ test/test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+def testing(my_name):
+    """
+    Parameters
+    ----------
+    my_name : string
+        Your Name
+
+    Returns
+    -------
+    None.
+
+    """
+    print(f"I'll convert a notebook for you some day, {my_name}.")
