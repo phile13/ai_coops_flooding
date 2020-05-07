a8ba62de7decb8ae9305987b0cc1862ca8c4748a
phile13 <47676503+phile13@users.noreply.github.com>
2020-05-07 14:23:21 -0400

    final test

---
+++ ai_coops/ai_coops.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+def print_name(myname):
+    """
+    print_name(myname) -- Prints out a string with users name in it
+
+    Parameters
+    ----------
+    myname : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    print("hi: %s" % (myname))
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as readme_file:
 requirements = ["requests>=2"]
 
 setup(
-    name="ai_coops_flooding",
+    name="ai_coops",
     version="0.0.1",
     author="Erick DiFiore",
     author_email="47676503+phile13@users.noreply.github.com",
