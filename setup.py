8d536bb43fb40b2cccc1b7346f49e4ddbe76af5b
phile13 <47676503+phile13@users.noreply.github.com>
2020-05-07 12:55:40 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="test",
+    version="0.0.1",
+    author="Erick DiFiore",
+    author_email="47676503+phile13@users.noreply.github.com",
+    description="A Package for the webcam flooding project with NOAA CO-OPS",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phile13/ai_coops_flooding",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
