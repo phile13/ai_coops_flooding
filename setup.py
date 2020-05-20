38316125d88a9b88ac6cc800d09ad41bf9e020ad
phile13 <47676503+phile13@users.noreply.github.com>
2020-05-20 14:30:07 -0400

    fixed setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description="A Package for projects with NOAA CO-OPS",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/phile13/ai_coops_flooding",
+    url="https://github.com/phile13/ai_coops",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
